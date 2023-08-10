238936353fab7f19700e8f5e4fefd2f249b41bed
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-10 12:08:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-/**
+ /**
  * main - Entry point
  * Description: Prints the specified text to the standard error.
  * Return: Always 1 (Failure)
