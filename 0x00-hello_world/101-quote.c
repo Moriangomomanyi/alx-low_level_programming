4192c33e8d97857851c7b31fcbea0029c4538e5f
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-10 11:42:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the specified text to the standard error.
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 59);
+return (1);
+}
