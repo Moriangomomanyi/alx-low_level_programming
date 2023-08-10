32e91f64a0f35a46235133901990c860655eb12a
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-10 11:24:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Description: Prints the text "Programming is like building a multilingual puzzle,"
  * followed by a new line using the `puts` function, and returns 0.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
