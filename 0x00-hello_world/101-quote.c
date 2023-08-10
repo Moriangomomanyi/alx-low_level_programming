b2cef210224c8796f28e12143b7989dbed5aae7f
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-10 11:59:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 /**
- * main . Prints the specified text to the standard error.
+ * main - Entry point
+ *
+ * Description: Prints the specified text to the standard error.
+ *
  * Return: Always 1 (Failure)
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 59);
-return (1);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 59);
+	return (1);
 }
