208740ac5c6dec2ee302c6b22ea4abdff869314f
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-11 06:44:39 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,22 +2,19 @@
 
 /**
  * main - Entry point
+ *
  * Description: Prints single-digit numbers of base 10 starting from 0 to 9,
- * followed by a new line, using the `putchar` function.
+ * followed by a new line, using only two `putchar` functions.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		putchar(number + '0');
+
 	putchar('\n');
 	return (0);
 }
