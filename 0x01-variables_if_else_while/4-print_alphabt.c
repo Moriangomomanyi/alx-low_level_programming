bdc9048f6a75b3d187823c8b93f0897eec833e05
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-11 05:33:19 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Description: Prints the alphabet in lowercase, excluding the letters 'q' and 'e',
+ * Desciption: Prints the alphabet in lowercase, excluding the letters 'q' and 'e',
  * using the `putchar` function.
  * Return: Always 0 (Success)
  */
