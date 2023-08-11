969f484dd81f4b2f6cc5d638837fa5dde1849aeb
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-11 07:06:21 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,6 @@ int main(void)
 		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			putchar(num1 + '0');
-			putchar(',');
-			putchar(' ');
 			putchar(num2 + '0');
 
 			if (!(num1 == 8 && num2 == 9))
