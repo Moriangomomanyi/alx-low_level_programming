958537a3596177d665705cfe0f5080312eabdee6
Moriangomomanyi <moriangomomanyi02@gmail.com>
2023-08-15 08:42:32 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,23 @@
-#include "holberton.h"
+/*
+ * File: is_lower.c
+ * Author: Your Name
+ * Description: A function to check if a character is lowercase.
+ */
 
 /**
-  * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: 1 for lowercase character or 0 for anything else
-  */
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-
-	return (0);
+    /**
+     * ASCII values for lowercase letters range from 'a' (97) to 'z' (122).
+     * Check if the provided character falls within this range.
+     */
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    else
+        return 0;
 }
