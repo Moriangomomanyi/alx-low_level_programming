531b15498dc36cf5f70799257a1198571b9a3b83
moriangomomanyi <moriangomomanyi02@gmail.com>
2023-11-08 04:49:04 -0800

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * print_opcodes - Print opcodes of the main function
+ * @bytes: Number of bytes to print
+ */
+void print_opcodes(int bytes)
+{
+	asm(
+			"main:                 \n"
+			"   push   %rbp         \n"
+			"   mov    %rsp,%rbp    \n"
+			"   sub    $0x30,%rsp   \n"
+			"   mov    %edi,-0x24(%rbp)\n"
+			"   mov    -0x24(%rbp),%eax\n"
+			"   test   %eax,%eax    \n"
+			"   jns    400694 <main+0x4> \n"
+			"   lea    0x2c9(%rip),%rdi\n"
+			"   call   400550 <puts@plt> \n"
+			"   mov    $0x2,%eax    \n"
+			"   jmp    400696 <main+0x6> \n"
+			"   mov    $0x0,%eax    \n"
+			"   mov    %ebp,%rsp    \n"
+			"   pop    %rbp         \n"
+			"   ret                 \n"
+			"   nopl   0x0(%rax)    \n"
+	:
+	:
+	: "rax", "rdi"
+	);
+
+	unsigned char *func_ptr = (unsigned char *)print_opcodes;
+	int i;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", func_ptr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 1 on incorret, 2 on negative bytes
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
-
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return (1);
 	}
+	int bytes = atoi(argv[1]);
+
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(bytes);
 
 	return (0);
 }
