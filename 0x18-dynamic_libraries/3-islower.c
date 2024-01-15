3ddfca97005063e5d6d970547e030b0129cf184f
Moriangomomanyi <moriangomomanyi02@gmail.com>
2024-01-15 12:55:12 -0800

    update

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,23 @@
+/*
+ * File: is_lower.c
+ * Author: Your Name
+ * Description: A function to check if a character is lowercase.
+ */
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+    /**
+     * ASCII values for lowercase letters range from 'a' (97) to 'z' (122).
+     * Check if the provided character falls within this range.
+     */
+if (c >= 'a' && c <= 'z')
+
+return (1);
+else
+return (0);
+}
--- 0x18-dynamic_libraries/build_libdynamic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc -c -Wall -Werror -fpic *.c
-gcc -shared -o libdynamic.so *.o
-rm *.o
-
-echo "Dynamic library libdynamic.so created successfully."
